d0565bc72da4cb7616a48541933d7933e6bdb445
LUIS FERNANDO MENDES DE OLIVEIRA ARAUJO <luisfemoa@gmail.com>
2026-02-08 13:50:42 -0300

    Aula02 PHP
    Aula sobre Formulário em php, usando o herd e o insomnia.

--- processa.php
+++ processa.php
@@ -1,17 +1,32 @@
 <?php
 
-$id = $_POST['id'];
-$titulo = $_POST['titulo'];
-$preco = $_POST['preco'];
-$descricao = $_POST['descricao'];
+$arquivo = 'users.json';
 
-$id_seguro = htmlspecialchars($id);
-$titulo_seguro = htmlspecialchars($titulo);
-$preco_seguro = htmlspecialchars($preco);
-$desc_seguro = htmlspecialchars($descricao);
 
-echo "ID recebido: " . $id_seguro . "<br>";
-echo "Título recebido: " . $titulo_seguro . "<br>";
-echo "Preço recebido: " . $preco_seguro . "<br>";
-echo "Descrição recebida: " . $desc_seguro;
+$json_atual = file_get_contents($arquivo);
+$usuarios = json_decode($json_atual, true); 
+if (!$usuarios) { $usuarios = []; }
+
+
+$ultimo_id = 0;
+foreach ($usuarios as $u) {
+    if ($u['id'] > $ultimo_id) { $ultimo_id = $u['id']; }
+}
+$novo_id = $ultimo_id + 1;
+
+$novo = [
+    "id" => $novo_id,
+    "username" => $_POST['username'],
+    "email" => $_POST['email'],
+    "password" => $_POST['password'],
+    "name" => [
+        "firstname" => $_POST['firstname'],
+        "lastname" => $_POST['lastname']
+    ]
+];
+
+$usuarios[] = $novo;
+file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT));
+
+echo "Foi Salvo graças a Deus!  " . $novo_id;
 ?>
